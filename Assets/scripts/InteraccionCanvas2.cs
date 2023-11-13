a241b7c5d80d2ca8c4e08db4ffef5836259bc079
bradymelendez <105991377+bradymelendez@users.noreply.github.com>
2023-11-13 08:58:38 -0500

    mejoras de dialogo

---
+++ Assets/scripts/InteraccionCanvas2.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteraccionCanvas2 : MonoBehaviour
+{
+    public Canvas canvas;
+    private bool dentroDelColisionador = false;
+    private bool canvasActivo = false;
+    private bool esperandoSegundaPulsacion = false;
+
+    void Start()
+    {
+        canvas.enabled = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    void Update()
+    {
+        if (dentroDelColisionador && Input.GetKeyDown(KeyCode.E))
+        {
+            if (canvasActivo)
+            {
+                if (esperandoSegundaPulsacion)
+                {
+                    DesactivarCanvas();
+                    esperandoSegundaPulsacion = false;
+                }
+                else
+                {
+                    esperandoSegundaPulsacion = true;
+                }
+            }
+            else
+            {
+                ActivarCanvas();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dentroDelColisionador = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dentroDelColisionador = false;
+            DesactivarCanvas();
+        }
+    }
+
+    void ActivarCanvas()
+    {
+        canvasActivo = true;
+        Time.timeScale = 0; 
+        canvas.enabled = true; 
+        Cursor.visible = true; 
+        Cursor.lockState = CursorLockMode.None; 
+    }
+
+    void DesactivarCanvas()
+    {
+        canvasActivo = false;
+        Time.timeScale = 1; 
+        canvas.enabled = false; 
+        esperandoSegundaPulsacion = false;
+        Cursor.visible = false; 
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+}
