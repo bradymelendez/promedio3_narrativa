39d44060eebc6fceb4d463c8a018fc4767352fa6
bradymelendez <105991377+bradymelendez@users.noreply.github.com>
2023-11-14 00:04:10 -0500

    mejora al dialogo del loco

--- Assets/scripts/ActivarTextoConCollider.cs
+++ Assets/scripts/ActivarTextoConCollider.cs
@@ -6,15 +6,27 @@ public class ActivarTextoConCollider : MonoBehaviour
 {
     public TextMeshProUGUI textoAActivar;
     public GameObject panelAActivar;
+    public float tiempoEntreLetras = 0.1f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        { 
-            textoAActivar.gameObject.SetActive(true);
+        {
             panelAActivar.SetActive(true);
-
-            Destroy(gameObject);
+            StartCoroutine(MostrarTextoPorLetras(textoAActivar.text));
         }
+    }
+
+    private IEnumerator MostrarTextoPorLetras(string texto)
+    {
+        textoAActivar.text = "";
+
+        foreach (char letra in texto)
+        {
+            textoAActivar.text += letra;
+            yield return new WaitForSeconds(tiempoEntreLetras);
+        }
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
--- Assets/scripts/Destroy.cs
+++ Assets/scripts/Destroy.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     public float time;
-    // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject,time);
