ffd0d7f65f1dbffdca19777a06003ffc3aeac64d
DiegoSValderrama <105023447+DiegoSValderrama@users.noreply.github.com>
2023-11-19 02:43:11 -0500

    enemigo garaje

--- Assets/scripts/Enemigo.cs
+++ Assets/scripts/Enemigo.cs
@@ -6,21 +6,42 @@ public class Enemigo : MonoBehaviour
 {
     public float vidaInicial = 100f;
     public float vidaActual;
-    public float dañorecibido = 20;
-
+    public float dañoRecibido = 20;
+    public Transform jugador;
+    public float rangoDeSeguimiento = 10f;
+    public float velocidad = 5f;
+    public float tiempoEntreGolpes = 2f;
+    public float distanciaRetroceso = 2f; // Distancia de retroceso cuando toca al jugador
+    private bool puedeHacerDaño = true;
+    private bool retrocediendo = false; // Variable para controlar si el enemigo está retrocediendo
 
     void Start()
     {
         vidaActual = vidaInicial;
     }
 
+    void Update()
+    {
+        if (!retrocediendo)
+        {
+            SeguirJugador();
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Roca"))
         {
-            RecibirDanio(dañorecibido);
+            RecibirDanio(dañoRecibido);
+        }
+        else if (other.CompareTag("Player") && puedeHacerDaño)
+        {
+            Retroceder();
+            // Inicia el temporizador
+            StartCoroutine(ActivarTemporizador());
         }
     }
+
     public void RecibirDanio(float cantidadDanio)
     {
         vidaActual -= cantidadDanio;
@@ -29,6 +50,15 @@ public class Enemigo : MonoBehaviour
         {
             Morir();
         }
+
+        if (puedeHacerDaño)
+        {
+            // Aplica el daño al jugador
+            // Agrega aquí tu lógica para causar daño al jugador
+
+            // Inicia el temporizador
+            StartCoroutine(ActivarTemporizador());
+        }
     }
 
     void Morir()
@@ -36,4 +66,46 @@ public class Enemigo : MonoBehaviour
         Debug.Log("El enemigo ha muerto");
         Destroy(gameObject);
     }
+
+    void SeguirJugador()
+    {
+        float distanciaAlJugador = Vector3.Distance(transform.position, jugador.position);
+
+        if (distanciaAlJugador <= rangoDeSeguimiento)
+        {
+            Vector3 direccionAlJugador = (jugador.position - transform.position).normalized;
+            transform.Translate(direccionAlJugador * velocidad * Time.deltaTime);
+        }
+    }
+
+    void Retroceder()
+    {
+        retrocediendo = true;
+        Vector3 direccionRetroceso = (transform.position - jugador.position).normalized;
+        transform.Translate(direccionRetroceso * distanciaRetroceso);
+    }
+
+    // Corrutina para activar el temporizador
+    IEnumerator ActivarTemporizador()
+    {
+        // Desactiva la posibilidad de hacer daño
+        puedeHacerDaño = false;
+
+        // Espera el tiempo especificado
+        yield return new WaitForSeconds(tiempoEntreGolpes);
+
+        // Activa la posibilidad de hacer daño nuevamente
+        puedeHacerDaño = true;
+
+        // Reinicia la variable de retroceso
+        retrocediendo = false;
+    }
+
+    // Método para visualizar el rango de seguimiento en el Editor de Unity
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangoDeSeguimiento);
+    }
 }
+
--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform jugador; // Asigna el objeto del jugador al que el enemigo seguirá
+    public float rangoDeSeguimiento = 10f; // Rango dentro del cual el enemigo comenzará a seguir al jugador
+    public float velocidad = 5f; // Velocidad de movimiento del enemigo
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calcula la distancia entre el enemigo y el jugador
+        float distanciaAlJugador = Vector3.Distance(transform.position, jugador.position);
+
+        // Verifica si el jugador está dentro del rango de seguimiento
+        if (distanciaAlJugador <= rangoDeSeguimiento)
+        {
+            // Calcula la dirección hacia la cual el enemigo debe moverse para seguir al jugador
+            Vector3 direccionAlJugador = (jugador.position - transform.position).normalized;
+
+            // Mueve al enemigo en la dirección calculada a una velocidad constante
+            transform.Translate(direccionAlJugador * velocidad * Time.deltaTime);
+        }
     }
 }
