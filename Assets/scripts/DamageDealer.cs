5acf3ef6f89bc706ab9e4c4416303582ce663e69
DiegoSValderrama <105023447+DiegoSValderrama@users.noreply.github.com>
2023-12-04 15:54:37 -0500

    g4y

--- Assets/scripts/DamageDealer.cs
+++ Assets/scripts/DamageDealer.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    public int damageAmount = 1; 
+    public int damageAmount = 1;
+    public float rotationSpeed = 5f;  // Velocidad de rotación hacia el jugador
+    private Transform target;  // Referencia al jugador
+
+    private void Start()
+    {
+        // Busca al jugador al inicio
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Player player = other.GetComponent<Player>(); 
+            Player player = other.GetComponent<Player>();
 
             if (player != null)
             {
-                player.TakeDamage(damageAmount); 
+                player.TakeDamage(damageAmount);
             }
         }
     }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            // Calcula la dirección hacia el jugador
+            Vector3 directionToTarget = target.position - transform.position;
+            directionToTarget.y = 0f;  // Mantiene la rotación en el plano horizontal
+
+            // Calcula la rotación para mirar hacia el jugador
+            Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
+
+            // Suaviza la rotación para que no sea brusca
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
 }
+
