58bda0ba7c95777dff01d8748ef0866348837333
bradymelendez <105991377+bradymelendez@users.noreply.github.com>
2023-12-04 22:01:46 -0500

    animación de doctora y transición

--- Assets/scripts/Teletransporte.cs
+++ Assets/scripts/Teletransporte.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class Teletransporte : MonoBehaviour
 {
     public Transform destino;
+    public GameObject pantalla ;
+    public void Start()
+    {
+        pantalla.SetActive(false);
+    }
     public void MoverJugadorAlDestino()
     {
         if (destino != null)
@@ -15,10 +20,11 @@ public class Teletransporte : MonoBehaviour
             Player.position = destino.position;
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            pantalla.SetActive(true);
         }
         else
         {
-            Debug.LogError("No se ha asignado un destino en el Inspector.");
+            Debug.LogError("No se ha asignado un destino en el Inspector.");      
         }
     }
 }
